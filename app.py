2ef3b14931887f30046649502e425c8e06cd5bad
s36240027-cpu <s36240027@student.ubm.ac.id>
2025-12-14 22:17:44 +0700

    app.py

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ model.fit(X_train_scaled, y_train)
 # ======================
 # EVALUATION
 # ======================
-threshold = 0.35
+threshold = 0.36
 
 proba = model.predict_proba(X_test_scaled)[:, 1]
 preds = (proba >= threshold).astype(int)
